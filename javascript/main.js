da205b058deeead3dcf1018f5c647d55227a86d7
Gourav Kumar <gouravkumarat@gmail.com>
2022-12-30 01:17:39 +0530

    update :  added the JS file

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,25 @@
-let button = document.querySelectorAll(".all-buttons")
+let string = ""
+let buttons = document.querySelectorAll(".all-buttons")
+Array.from(buttons).map((button) => {
+    button.addEventListener('click', (e) => {
+        let userInput = e.target.innerHTML
+
+
+
+        if (userInput == "=") {
+            string = eval(string);
+            document.querySelector("input").value = string;
+        }
+        else if (userInput == "C") {
+            string = ""
+            document.querySelector("input").value = string;
+        }
+
+        else {
+            string = string + userInput;
+            document.querySelector("input").value = string;
+
+        }
+
+    })
+})
